1b78abfac6d87c3183315e44d67de3204be6ac02
Oscar Lopez <oscar.lopez@epicgames.com>
2024-02-28 01:36:03 -0500

    Minecraft Server initial setup

--- config.tf
+++ config.tf
@@ -1,11 +1,14 @@
 terraform {
   required_version = "1.7.4"
-  backend "s3" {
-    bucket = "osloor-terraform-states"
-    key    = "minecraft-server"
-    region = "us-east-1"
-  }
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Kroz"
 
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "minecraft-server-terraform"
+    }
+  }
 }
 
 provider "aws" {
