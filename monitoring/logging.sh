f9ff4642b02b90e619b6979cbb7615990d686ca4
Oscar Lopez <oscar.lopez@epicgames.com>
2024-03-08 10:51:03 -0500

    Funciona profavor

--- monitoring/logging.sh
+++ monitoring/logging.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i '/Users/Oscar_Lopez/Downloads/test.pem' ubuntu@3.87.206.83 tail -f /minecraft_server/logs/latest.log
+ssh -i '/Users/Oscar_Lopez/Downloads/test.pem' ubuntu@44.212.43.186 tail -f /minecraft_server/logs/latest.log
